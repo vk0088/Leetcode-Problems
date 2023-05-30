ca4208b9193d1c7f9ce4e7dbac57bc16853a4555
V K Akshat <109818773+vk0088@users.noreply.github.com>
2023-05-30 21:57:57 +0530

    Update Roman to Integer.py

--- Roman to Integer.py
+++ Roman to Integer.py
@@ -1,13 +1,16 @@
+class Solution:
     def romanToInt(self, s: str) -> int:
-        ROMANS = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        number = 0
-      
-        for i in range(len(s) - 1):
-            if ROMANS[s[i + 1]]  > ROMANS[s[i]]:
-                number -= ROMANS[s[i]]
+        maps = {'I' : 1,'V' : 5,'X' : 10,
+        'L' : 50,'C' : 100,'D' : 500,'M' : 1000}
+        sums = 0
+        i = 0
+        while i < len(s)-1:
+            if maps[s[i]] < maps[s[i+1]]:
+                sums += maps[s[i+1]]-maps[s[i]]
+                i += 1
             else:
-                number += ROMANS[s[i]]
-                
-        number += ROMANS[s[len(s) - 1]]
-        
-        return number
+                sums += maps[s[i]]
+            i += 1
+        if i != len(s):
+            sums += maps[s[-1]]
+        return sums
