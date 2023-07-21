ab26eb48334b49ca765d96d758faaf5f0b87e307
V K Akshat <109818773+vk0088@users.noreply.github.com>
2023-07-21 23:04:49 +0530

    Update Number of Longest Increasing Subsequence.java

--- Number of Longest Increasing Subsequence.java
+++ Number of Longest Increasing Subsequence.java
@@ -23,7 +23,7 @@ class Solution {
             }
             maxLength = Math.max(maxLength, dp[i]);
         
-
+        }
         int result = 0;
         for (int i = 0; i < n; i++) {
             if (dp[i] == maxLength) {
