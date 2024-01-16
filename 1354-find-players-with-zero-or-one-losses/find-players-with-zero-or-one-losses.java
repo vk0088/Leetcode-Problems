74ce1125185b641605808c9d190e3a92eab9706d
V K Akshat <109818773+vkakshhat@users.noreply.github.com>
2024-01-17 00:16:02 +0530

    Time: 333 ms (5.40%) | Memory: 113.7 MB (5.03%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,24 +1,22 @@
 class Solution {
-    public List<List<Integer>> findWinners(int[][] arr) {
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        res.add(new ArrayList<>());
-        int a[] = new int[100001];// winner
-        int b[] = new int[100001];// loser
-        for (int p[] : arr) {
-            a[p[0]]++;
-            b[p[1]]++;
+    public List<List<Integer>> findWinners(int[][] matches) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for (int n[] : matches) {
+            if (!map.containsKey(n[0]))
+                map.put(n[0], 0);
+            map.put(n[1], map.getOrDefault(n[1], 0) + 1);
         }
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] > 0 && b[i] == 0) {
-                res.get(0).add(i);
-            }
+        List<Integer> win = new ArrayList<>();
+        List<Integer> lost = new ArrayList<>();
+        for (int n : map.keySet()) {
+            if (map.get(n) == 0)
+                win.add(n);
+            if (map.get(n) == 1)
+                lost.add(n);
         }
-        for (int i = 0; i < b.length; i++) {
-            if (b[i] == 1) {
-                res.get(1).add(i);
-            }
-        }
-        return res;
+        list.add(win);
+        list.add(lost);
+        return list;
     }
 }
