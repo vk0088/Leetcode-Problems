8d58b47088bbcc74b3fab8bc69ae7d2f2ef7f7ce
V K Akshat <109818773+vkakshhat@users.noreply.github.com>
2024-04-20 22:04:58 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Union of Two Sorted Arrays/union-of-two-sorted-arrays.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def findUnion(self,arr1,arr2,n,m):
+        return list(sorted(set(arr1).union(set(arr2))))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Contributed by : Nagendra Jha
+# Modified by : Sagar Gupta
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,m = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+        b = list(map(int,input().strip().split()))
+        ob=Solution()
+        li = ob.findUnion(a,b,n,m)
+        for val in li:
+            print(val, end = ' ')
+        print()
+# } Driver Code Ends
